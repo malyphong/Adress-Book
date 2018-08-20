84aad1a9a8b45743350de73160ad5c0282405099
Dustin Starke and Nikki Wong <dustin@>
2018-08-20 15:10:19 -0700

    add an address prototype to create associations between the object name and object address

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,17 @@ function Address(street, city, state) {
   Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
   }
-
+  Address.prototype.fullAddress = function() {
+    return this.street + ", " + this.city + ", " + this.state;
+  }
 // user interface logic
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+}
 $(document).ready(function() {
 
   $("#add-address").click(function() {
@@ -57,15 +66,9 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-
+    resetFields()
   });
 });
